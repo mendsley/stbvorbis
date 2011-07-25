4f8a8771dfac8ec9728eb8d7b85e8aa2a890dfeb
bkaradzic <bkaradzic@gmail.com>
2011-07-25 22:04:32 +0000

    Fixed GCC errors.

--- stb_vorbis.c
+++ stb_vorbis.c
@@ -2375,7 +2375,7 @@ void dct_iv_slow(float *buffer, int n)
          //acc += x[j] * cos(M_PI / n * (i + 0.5) * (j + 0.5));
       buffer[i] = acc;
    }
-   free(x);
+// BK - wtf - error: attempt to free a non-heap object 'x'   free(x);
 }
 
 void inverse_mdct_slow(float *buffer, int n, vorb *f, int blocktype)
