5130e5e9e8ffea8b3d55493b1d4ed70fa1d5237d
Matthew Endsley <mendsley@gmail.com>
2012-06-25 10:27:02 +0000

    fixing an if-bracing warning that clang treats as fatal

--- stb_vorbis.c
+++ stb_vorbis.c
@@ -1479,8 +1479,10 @@ static int next_segment(vorb *f)
 static int get8_packet_raw(vorb *f)
 {
    if (!f->bytes_in_seg)
+   {
       if (f->last_seg) return EOP;
       else if (!next_segment(f)) return EOP;
+   }
    assert(f->bytes_in_seg > 0);
    --f->bytes_in_seg;
    ++f->packet_bytes;
